e4967c308083a4beabdcf954d46c50b6ae8f7e32
manzi5404 <manziluckyun@gmail.com>
2026-07-20 10:30:45 +0200

    Fix: installed needed packeges for admin deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    vue(),
+    tsconfigPaths()
+  ],
   server: {
     proxy: {
       '/api': {
